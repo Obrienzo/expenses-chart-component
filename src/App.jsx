5c97df43e9726b3b822298beb45193cd3bd72b6b
Disney <disneyconstant996@gmail.com>
2025-06-22 11:13:41 +0300

    Added conditional for graph color variation

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,32 @@ import DayData from "./DayData";
 
 function App() {
   const [ spendings, setSpendings ] = useState([]);
+  const [ greatValue, setGreatValue ] = useState(0);
 
   useEffect(() => {
     axios
       .get("http://localhost:3001/expenses")
       .then(response => {
-        // console.log(response.data);
         setSpendings(response.data);
+        const max = Math.max(...response.data.map(item => item.amount));
+        setGreatValue(max);
       })
   }, [])
+
 
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-red-100">
       <article className="max-w-80 m-5 h-fit">
-        <section className="text-neutral-50 flex justify-between bg-red-500 p-5 mb-5 rounded-lg">
+        <section className="text-neutral-50 flex justify-between items-center bg-red-500 p-5 mb-2.5 rounded-lg">
           <div>
-            <p className="font-light text-xs">My balance</p>
-            <p className="font-medium text-xl">$921.48</p>
+            <p className="font-light text-sm">My balance</p>
+            <p className="font-medium text-2xl">$921.48</p>
           </div>
-          <svg width="72" height="48" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#382314" cx="48" cy="24" r="24"/><circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23"/></g></svg>
+          <svg width="65" height="35" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#382314" cx="48" cy="24" r="24"/><circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23"/></g></svg>
         </section>
         <section className="bg-neutral-50 p-5 rounded-lg">
-          <h1 className="mb-8 text-brown-950 font-medium text-xl">Spending - Last 7 days</h1>
-          {/* Adding  the component of daily expenses for the week */}
+          <h1 className="mb-8 text-brown-950 font-medium text-xl sm:text-2xl">Spending - Last 7 days</h1>
           <ul className="grid grid-cols-7 gap-3.5">
             {
               spendings.map(spending =>
@@ -35,17 +37,18 @@ function App() {
                   key={spending.id}
                   amount={spending.amount}
                   day={spending.day}
+                  greatValue={greatValue}
                 />
               )
             }
           </ul>
-          <div className="mt-7 pt-5 flex justify-between items-end text-xs sm:text-sm font-normal text-brown-400 border-t border-t-zinc-200">
+          <div className="mt-7 pt-5 flex justify-between items-end text-xs sm:text-sm font-normal text-brown-400 border-t border-t-zinc-300">
             <div>
               <p>Total this month</p>
               <h2 className="font-medium text-brown-950 text-4xl">$478.33</h2>
             </div>
             <div className="text-end">
-              <p className="text-brown-950 text-sm font-medium">+2.4%</p>
+              <p className="text-brown-950 text-sm font-bold">+2.4%</p>
               <p>from last month</p>
             </div>
           </div>
--- src/DayData.jsx
+++ src/DayData.jsx
@@ -1,18 +1,21 @@
 
 
-function DayData({ amount, day }) {
+function DayData({ amount, day, greatValue }) {
 
     return (
         <li className="text-center">
             <div className="h-24 w-full flex items-end mb-2">
-                <div title={`$${amount}`} className="bg-red-500 w-full rounded-sm cursor-pointer" style={{ height: `${amount / 10 }rem` }}>
+                <div title={`$ ${amount}`} className={` w-full rounded-sm cursor-pointer ${greatValue === amount ? 'bg-blue-300' : 'bg-red-500'}`} style={{ height: `${amount / 10 }rem` }}>
                 </div>
             </div>
             <div>
-                <p className="text-brown-400 text-xs sm:text-sm">{day}</p>
+                <p className="text-brown-400 text-xs font-light sm:text-sm">{day}</p>
             </div>
         </li>
     );
 }
 
 export default DayData;
+
+
+// Next Learn how to use DOM manipulation in React to display the amount of each instead of using the title attribute..
