78da617fd83b8208032998efc4550d438adac3e6
Disney <disneyconstant996@gmail.com>
2025-06-22 12:28:17 +0300

    Added component interactivity

--- src/DayData.jsx
+++ src/DayData.jsx
@@ -1,15 +1,26 @@
-
+import { useState } from "react";
 
 function DayData({ amount, day, greatValue }) {
+    const [ showTooltip, setShowTooltip ] = useState(false);
 
     return (
         <li className="text-center">
             <div className="h-24 w-full flex items-end mb-2">
-                <div title={`$ ${amount}`} className={` w-full rounded-sm cursor-pointer ${greatValue === amount ? 'bg-blue-300' : 'bg-red-500'}`} style={{ height: `${amount / 10 }rem` }}>
+                <div 
+                    className={`relative w-full rounded-sm cursor-pointer ${greatValue === amount ? 'bg-blue-300' : 'bg-red-500'}`} 
+                    style={{ height: `${amount / 10 }rem` }}
+                    onMouseEnter={() => setShowTooltip(true)}
+                    onMouseLeave={() => setShowTooltip(false)}
+                >
+                    <code className={`${showTooltip ? 'block' : 'hidden'} z-10 shadow shadow-gray-900 text-xs bg-brown-950 rounded-sm text-red-100 absolute -top-6 -left-2.5 w-fit py-0.5 px-1`}>
+                        ${amount}
+                    </code>
                 </div>
             </div>
             <div>
-                <p className="text-brown-400 text-xs font-light sm:text-sm">{day}</p>
+                <p className="text-brown-400 text-xs font-light sm:text-sm">
+                    {day}
+                </p>
             </div>
         </li>
     );
@@ -17,5 +28,3 @@ function DayData({ amount, day, greatValue }) {
 
 export default DayData;
 
-
-// Next Learn how to use DOM manipulation in React to display the amount of each instead of using the title attribute..
